377b3b30e61234973dc64a59f38ecebd7546e57d
Tim Franssen <tim.franssen@nedap.com>
2019-11-27 16:49:07 +0100

    Fix tiny bug where the error message errors 🙈

--- irma-core/state-machine.js
+++ irma-core/state-machine.js
@@ -30,7 +30,7 @@ module.exports = class StateMachine {
   _getNewState(transition) {
     let newState = transitions[this._state][transition];
     if (!newState) newState = transitions[this._state]['fail'];
-    if (!newState) throw new Error(`Invalid transition '${t}' from state '${this._state}' and could not find a "fail" transition to fall back on.`);
+    if (!newState) throw new Error(`Invalid transition '${transition}' from state '${this._state}' and could not find a "fail" transition to fall back on.`);
     return newState;
   }
 
