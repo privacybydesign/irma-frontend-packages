7c784c8ec7552be846913056df61d23a0b19788f
Tim Franssen <tim.franssen@nedap.com>
2019-11-27 13:30:13 +0100

    Use new features of irma-web in the example

--- examples/browser/irma-web/index.js
+++ examples/browser/irma-web/index.js
@@ -6,7 +6,12 @@ const Dummy      = require('irma-dummy');
 
 const irma = new IrmaCore({
   debugging: true,
-  element:   '#irma-web-form'
+  element:   '#irma-web-form',
+  language:  'en',
+  translations: {
+    header:  'Sign the agreement with <i class="irma-web-logo">IRMA</i>',
+    loading: 'Just one second please!'
+  }
 });
 
 irma.use(IrmaWeb);
