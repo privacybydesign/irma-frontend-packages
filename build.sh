846173c2ff86d81e20ccfd547431dadb62295d82
Ivar Derksen <ivarderksen@hotmail.nl>
2020-02-27 15:40:28 +0100

    Build script relied on execution order, so made this explicit

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 
 # Script makes sure that all modules that need building steps are built from source if needed
 
-packages=$(find . -name "package-lock.json" -not -path "*/node_modules/*" -not -path "*/examples/*")
-
 rootdir=$(pwd)
-for package in ""${packages[@]}""; do
-  cd "$rootdir" || exit
-  if git ls-files --error-unmatch "$package" &> /dev/null; then
-    directory=$(dirname "$package")
-    echo "Running 'npm install' and 'npm run release' for $directory"
-    cd "$directory" || (echo "Directory does not exit" & exit)
-    npm install
-    npm run release
-  fi
-done
+
+cd "$rootdir" || exit
+echo "Running 'npm install' and 'npm run release' for irma-css"
+cd irma-css || exit
+npm install
+npm run release
+
+cd "$rootdir" || exit
+echo "Running 'npm install' and 'npm run release' for irma-frontend"
+cd irma-frontend || exit
+npm install
+npm run release
